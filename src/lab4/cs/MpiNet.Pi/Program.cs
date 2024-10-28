c3df2c2f02a90f8278d1b8cb9f82a7bd9ca5189b
entityfx <entityfx@ya.ru>
2024-10-28 21:26:14 +0300

    Aded lab 4 and lab 5 sources

---
+++ src/lab4/cs/MpiNet.Pi/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics;
+using MPI;
+
+int dartsPerProcessor = 100000000;
+var sw = new Stopwatch();
+MPI.Environment.Run(ref args, comm =>
+{
+    if (args.Length > 0)
+        dartsPerProcessor = Convert.ToInt32(args[0]);
+
+    if (comm.Rank == 0)
+    {
+        sw.Start();
+    }
+
+    Random random = new Random(5 * comm.Rank);
+    int dartsInCircle = 0;
+    for (int i = 0; i < dartsPerProcessor; ++i)
+    {
+        double x = (random.NextDouble() - 0.5) * 2;
+        double y = (random.NextDouble() - 0.5) * 2;
+        if (x * x + y * y <= 1.0)
+            ++dartsInCircle;
+    }
+
+    int totalDartsInCircle = comm.Reduce(dartsInCircle, Operation<int>.Add, 0);
+    if (comm.Rank == 0)
+    {
+        var pi = 4.0 * totalDartsInCircle / (comm.Size * dartsPerProcessor);
+        Console.WriteLine($"Pi is approximately {pi:F15}.");
+        Console.WriteLine($"Elapsed {sw.Elapsed}.");
+    }
+});
+
---
+++ src/lab5/cs/Lab5.Network/Lab5.Network.TcpClient/Program.cs
@@ -0,0 +1,84 @@
+﻿using System.Numerics;
+using Lab5.Network.Common;
+using Lab5.Network.Common.UserApi;
+
+internal class Program
+{
+    private static object _locker = new object();
+
+    public static async Task Main(string[] args)
+    {
+        var serverAdress = new Uri("tcp://127.0.0.1:5555");
+        var client = new NetTcpClient(serverAdress);
+        Console.WriteLine($"Connect to server at {serverAdress}");
+        await client.ConnectAsync();
+
+        var userApi = new UserApiClient(client);
+        await ManageUsers(userApi);
+        client.Dispose();
+    }
+
+    private static async Task ManageUsers(IUserApi userApi)
+    {
+        PrintMenu();
+
+        while(true) {
+            var key = Console.ReadKey(true);
+
+            PrintMenu();
+
+            if (key.Key == ConsoleKey.D1) 
+            {
+                var users = await userApi.GetAllAsync();
+                Console.WriteLine($"| Id    |     Name        | Active |");
+                foreach (var user in users)
+                {
+                    Console.WriteLine($"| {user.Id,5} | {user.Name,15} | {user.Active,8} |");
+                }
+            }
+
+            if (key.Key == ConsoleKey.D2) 
+            {
+                Console.Write("Enter user id: ");
+                var userIdString = Console.ReadLine();
+                int.TryParse(userIdString, out var userId);
+                var user = await userApi.GetAsync(userId);
+                Console.WriteLine($"Id={user?.Id}, Name={user?.Name}, Active={user?.Active}");
+            }
+
+            if (key.Key == ConsoleKey.D3) 
+            {
+                Console.Write("Enter user name: ");
+                var addUserName = Console.ReadLine() ?? "empty";
+                var addUser = new User(Id: 0,
+                    Name : addUserName,
+                    Age : 30,
+                    Active : true
+                );
+                var addResult = await userApi.AddAsync(addUser);
+
+                Console.WriteLine(addResult ? "Ok" : "Error");
+            }
+
+            if (key.Key == ConsoleKey.Escape)
+            {
+                break;
+            }
+        }
+        Console.ReadKey();
+        //while (Console.Read)
+    }
+
+    private static void PrintMenu()
+    {
+        lock (_locker)
+        {
+            Console.WriteLine("1 - Get all users");
+            Console.WriteLine("2 - Get user by id");
+            Console.WriteLine("3 - Create user");
+            Console.WriteLine("4 - Update user");
+            Console.WriteLine("5 - Delete user");
+            Console.WriteLine("-------");
+        }
+    }
+}
